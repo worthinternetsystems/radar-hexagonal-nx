34a920e699cc37bc38be2be9630f38775f6998b7
Jonny <jwright@worth.systems>
2024-10-02 13:36:03 +0100

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/jCIF47q0Ho

---
+++ apps/radar-hexagonal-nx/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'radar-hexagonal-nx',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/radar-hexagonal-nx',
+};
